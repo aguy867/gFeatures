a3271bd21af0f8d8a117479dfdd8ed7996d4157b
EspiDev <aguy867@gmail.com>
2017-11-25 15:28:18 -0500

    test

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+let express = require('express');
+let app = express();
+
+app.get('/', function(req,res) {
+    res.send("Hello world!");
+});
+
 var server = require('http').createServer(app),
     io = require('socket.io').listen(server),
     cookie = require('cookie');
