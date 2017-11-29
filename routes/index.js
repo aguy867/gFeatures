e66e757aa860cae683047676923cd9c9db549755
EspiDev <aguy867@gmail.com>
2017-11-29 11:20:00 -0500

    push

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('loading',  {title: 'express'});
+    //setTimeout(() => res.render('index', {title: 'express'}), 3000);
 });
 
 module.exports = router;
