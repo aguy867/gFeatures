6eaa8286aa5412fb9ebf7b6da094688630c37973
EspiDev <aguy867@gmail.com>
2017-12-01 09:08:52 -0500

    404

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ let router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('loading',  {title: 'express'});
-    //setTimeout(() => res.render('index', {title: 'express'}), 3000);
+    setTimeout(() => res.render('index', {title: 'express'}), 3000);
 });
 
 module.exports = router;
